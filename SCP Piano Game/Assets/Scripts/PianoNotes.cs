64e407d1039f9c87411ff0975e6045e1b5d3c1a4
Ownerkubus <kubusdenouden@gmail.com>
2022-02-22 23:53:41 +0100

    Piano tiles working

---
+++ SCP Piano Game/Assets/Scripts/PianoNotes.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoNotes : MonoBehaviour
+{
+
+    public AudioSource A1_Note;
+    public AudioSource B1_Note;
+    public AudioSource C1_Note;
+    public AudioSource D1_Note;
+    public AudioSource E1_Note;
+    public AudioSource F1_Note;
+    public AudioSource G1_Note;
+
+    public AudioSource A2_Note;
+    public AudioSource B2_Note;
+    public AudioSource C2_Note;
+    public AudioSource D2_Note;
+    public AudioSource E2_Note;
+    public AudioSource F2_Note;
+    public AudioSource G2_Note;
+
+    public void A1_Note_Play()
+    {
+        A1_Note.Play();
+    }
+
+    public void B1_Note_Play()
+    {
+        B1_Note.Play();
+    }
+
+    public void C1_Note_Play()
+    {
+        C1_Note.Play();
+    }
+    public void D1_Note_Play()
+    {
+        D1_Note.Play();
+    }
+    public void E1_Note_Play()
+    {
+        E1_Note.Play();
+    }
+    public void F1_Note_Play()
+    {
+        F1_Note.Play();
+    }
+    public void G1_Note_Play()
+    {
+        G1_Note.Play();
+    }
+
+    public void A2_Note_Play()
+    {
+        A2_Note.Play();
+    }
+
+    public void B2_Note_Play()
+    {
+        B2_Note.Play();
+    }
+
+    public void C2_Note_Play()
+    {
+        C2_Note.Play();
+    }
+    public void D2_Note_Play()
+    {
+        D2_Note.Play();
+    }
+    public void E2_Note_Play()
+    {
+        E2_Note.Play();
+    }
+    public void F2_Note_Play()
+    {
+        F2_Note.Play();
+    }
+    public void G2_Note_Play()
+    {
+        G2_Note.Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
